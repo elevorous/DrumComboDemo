1eeaa09e3f69cb335f33f55f489097e3be672f61
Noah <n.mckenzie0296@gmail.com>
2024-04-07 17:15:07 +0100

    [DCO-39] Flat CSS to SCSS files
    - (WIP) Flat styles have been moved into separate SCSS files.
    - Gulpfile has been updated to include automatic sass compilation.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,10 +3,16 @@
 */
 var gulp = require("gulp");
 var preprocess = require("gulp-preprocess");
+var sass = require("gulp-dart-sass");
 
 const TEMPLATE_ROOT = "./templates/**/*.html";
 const TEMPLATE_PREPROCESS_SRC = "./templates/site/index.html";
 const TEMPLATE_PREPROCESS_DEST = "./";
+
+const SASS_SRC = "./scss/**/*.scss";
+const SASS_DEST = "./css";
+
+const SASS_MQ_INCLUDE_PATH = "./node_modules/sass-mq/_mq.scss";
 
 gulp.task("html", function() {
   return gulp.src(TEMPLATE_PREPROCESS_SRC)
@@ -18,4 +24,16 @@ gulp.task("watch-html", function() {
     return gulp.watch(TEMPLATE_ROOT, gulp.series("html"));
 });
 
-exports.default = gulp.series("html", "watch-html");
+gulp.task("sass", function() {
+    return gulp.src(SASS_SRC)
+                .pipe(sass({
+                    includePaths: [SASS_MQ_INCLUDE_PATH]
+                }).on('error', sass.logError))
+                .pipe(gulp.dest(SASS_DEST));
+});
+
+gulp.task("watch-sass", function() {
+    return gulp.watch(SASS_SRC, gulp.series("sass"));
+});
+
+exports.default = gulp.series("html", "sass", gulp.parallel("watch-html", "watch-sass"));
