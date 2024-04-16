77413b2f54125e938b579380a2d461c37735f233
Noah <n.mckenzie0296@gmail.com>
2024-04-16 16:11:07 +0100

    Gulpfile updated with dep on gulp-mode, plus start of GA integration
    - [DCO-80] - Gulpfile now makes use of unversioned integrations.js, which contains build mode specific vars for use in gulp-preprocess.
    - [DCO-17] - test for google analytics integration

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,15 @@
 /**
 * https://github.com/pioug/gulp-preprocess
 */
+var integrations = require("./integrations.js");
 var gulp = require("gulp");
 var preprocess = require("gulp-preprocess");
 var sass = require("gulp-dart-sass");
+var mode = require("gulp-mode")({
+    modes: ["gulpModeLive", "gulpModeDev"],
+    default: "gulpModeDev",
+    verbose: false
+});
 
 const TEMPLATE_ROOT = "./templates/**/*.html";
 const TEMPLATE_PREPROCESS_SRC = "./templates/site/index.html";
@@ -14,9 +20,16 @@ const SASS_DEST = "./css";
 
 const SASS_MQ_INCLUDE_PATH = "./node_modules/sass-mq/_mq.scss";
 
+const MODE_DEV = "--dev";
+const MODE_LIVE = "--live";
+
+const preprocessInstance = preprocess({
+    context: (mode.gulpModeLive() ? integrations.LIVE : integrations.DEV)
+});
+
 gulp.task("html", function() {
-  return gulp.src(TEMPLATE_PREPROCESS_SRC)
-                .pipe(preprocess())
+    return gulp.src(TEMPLATE_PREPROCESS_SRC)
+                .pipe(preprocessInstance)
                 .pipe(gulp.dest(TEMPLATE_PREPROCESS_DEST));
 });
 
